e847af9a41af0ec1aa1522f9028d561e1a373005
Chris Briggs <chrisbriggs@ssw.com.au>
2015-07-22 10:11:08 +1000

    Added SuperPowersWeatherAPI
    The web API used during the presentation

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# BneDotNet-Dev-superpowers-Jumping-into-Windows-IoT
-The demo code from BneDotNet Dev superpowers Jumping into Windows IoT
+# BneDotNet Dev superpowers Jumping into Windows IoT
+
+[Demo code from BneDotNet Dev superpowers Jumping into Windows IoT](http://blog.chrisbriggsy.com/Dev-superpowers-Jumping-into-windows-internet-of-things/)
+
+The only change I have made was to remove my Azure web API URL.
+
+This can be replaced by deploying the SuperPowersWeatherAPI project to Azure.
+
+Feel free to tweet me comments, feedback or questions to [@ChrisBriggsy](https://twitter.com/ChrisBriggsy).
+
---
+++ SuperPowersWeatherAPI/SuperPowersWeatherAPI/Controllers/weatherController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace SuperPowersWeatherAPI.Controllers
+{
+    public class WeatherController : ApiController
+    {
+        private static List<WeatherData> weatherData = new List<WeatherData>();
+
+
+        public List<WeatherData> Get()
+        {
+            return weatherData;
+        }
+
+        public void Post([FromBody]WeatherData value)
+        {
+            weatherData.Add(value);
+        }
+
+        public void Delete()
+        {
+            weatherData = new List<WeatherData>();
+        }
+    }
+
+    public class WeatherData
+    {
+        public string DeviceID { get; set; }
+        public float Humidity { get; set; }
+        public float Temperature { get; set; }
+    }
+}
